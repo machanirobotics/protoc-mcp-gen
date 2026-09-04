cf0440fd6be1813a31861bb1eadc95b0f959e66a
oh-tarnished <104291599+oh-tarnished@users.noreply.github.com>
2026-09-04 17:46:45 +0530

    chore: add copyright and license headers to multiple files

--- plugin/generator/schema_wkt.go
+++ plugin/generator/schema_wkt.go
@@ -1,3 +1,6 @@
+// Copyright 2026 The Protobuf Project authors.
+// SPDX-License-Identifier: Apache-2.0
+
 package generator
 
 // schema_wkt.go contains JSON Schema converters for protobuf map fields
